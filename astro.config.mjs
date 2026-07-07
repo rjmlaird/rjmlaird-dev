c21c3781523a02362ef2959423615d62ef0c7efe
rjmlaird <rjmlaird@gmail.com>
2026-07-07 12:04:44 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import path from "path";
 
 export default defineConfig({
+  site: "https://dev.rjmlaird.co.uk",
   vite: {
     resolve: {
       alias: {
