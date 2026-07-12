9866d3e2c3342fc41249eff244816eba7c7624c8
rjmlaird <rjmlaird@gmail.com>
2026-07-12 20:49:12 +0100

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import path from "path";
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://dev.rjmlaird.co.uk',
+  integrations: [sitemap()],
   vite: {
     resolve: {
       alias: {
